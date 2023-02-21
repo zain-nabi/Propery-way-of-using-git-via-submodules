450377083fe26c6098e0a9cedc594631c9d6b858
Zain Nabi <zainolnabi88@gmail.com>
2023-02-21 10:38:08 +0200

    Initial Commit
    Initial Commit

---
+++ BestValueDollar/TShirt/T-Shirt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestValueDollar.TShirt
+{
+    public class T_Shirt
+    {
+        public  int Id { get; set; }
+        public  string Colour { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+    }
+}
